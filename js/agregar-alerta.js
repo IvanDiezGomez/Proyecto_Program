54e6f56de77862a0b6f4de26ed59ff9fa3eddc3c
IvanDiezGomez <ivanox28@gmail.com>
2026-09-17 17:47:27 -0300

    se creo carpeta netifly para js

--- js/agregar-alerta.js
+++
@@ -1,6 +0,0 @@
-
-const btnAlerta = document.getElementById("btn-alerta");
-
-btnAlerta.addEventListener("click", () => {  
-    window.location.href="agregar-alerta.html";
-});  
--- js/index.js
+++
@@ -1,6 +0,0 @@
-const btn=document.getElementById("btn-login");
-
-
-btn.addEventListener("click",()=>{
-    window.location.href="login.html";
-});
